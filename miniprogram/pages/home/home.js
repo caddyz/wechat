31a0bf8e9266f098db6511b6831e6f16e7dbb2dd
kaiser <18784227116@163.com>
2019-04-28 18:47:12 +0800

    discover
    轮播

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -34,7 +34,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.onQuery();
   },
 
   /**
@@ -84,5 +84,56 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  /**
+   * 查询数据
+   */
+  onQuery: function () {
+    const db = wx.cloud.database()
+    // 查询当前用户所有的 counters
+    db.collection('discover').where({
+      _openid: getApp().globalData.openid
+    }).get({
+      success: res => {
+        console.log(res.data)
+        this.setData({
+          adList: res.data
+        })
+        console.log('[数据库] [查询记录] 成功: ', res)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询记录失败'
+        })
+        console.error('[数据库] [查询记录] 失败：', err)
+      }
+    })
+  },
+  onAdd: function () {
+    const db = wx.cloud.database()
+    db.collection('discover').add({
+      data: {
+        count: 1
+      },
+      success: res => {
+        // 在返回结果中会包含新创建的记录的 _id
+        this.setData({
+          counterId: res._id,
+          count: 1
+        })
+        wx.showToast({
+          title: '新增记录成功',
+        })
+        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '新增记录失败'
+        })
+        console.error('[数据库] [新增记录] 失败：', err)
+      }
+    })
+  },
 })
